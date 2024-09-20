219f6e57800de720f5f3dc3639a1b6186d3b9183
amandatrim <amanda162@usf.edu>
2024-09-20 06:56:24 +0100

    Initialize inventory with product objects

---
+++ customerOrderManagement.js
@@ -0,0 +1,10 @@
+// Task 1 
+// I created the inventory array, this array stores information about all the product object (coffee available) in the system.
+//This array contains product objects with properties for name, price, and quantity
+let inventory = [
+    {name: 'Americano', price: 5, quantity: 50},
+    {name: 'Cappuccino', price: 7, quantity: 100},
+    {name: 'Caffè macchiato', price: 8, quantity: 70},
+    {name: 'Latte', price: 6, quantity: 65},
+    {name: 'Espresso', price: 4, quantity: 85}
+]
