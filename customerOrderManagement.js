801253cf320d386f315bef48d150483b70a0685d
amandatrim <amanda162@usf.edu>
2024-09-20 07:01:46 +0100

    Initialize orders array

--- customerOrderManagement.js
+++ customerOrderManagement.js
@@ -1,10 +1,17 @@
-// Task 1 
-// I created the inventory array, this array stores information about all the product object (coffee available) in the system.
-//This array contains product objects with properties for name, price, and quantity
+/*  Task 1 
+   I created the inventory array, this array stores information about all the product object (coffee available) in the system.
+   This array contains product objects with properties for name, price, and quantity. 
+*/
 let inventory = [
     {name: 'Americano', price: 5, quantity: 50},
     {name: 'Cappuccino', price: 7, quantity: 100},
     {name: 'Caffè macchiato', price: 8, quantity: 70},
     {name: 'Latte', price: 6, quantity: 65},
     {name: 'Espresso', price: 4, quantity: 85}
-]
+];
+
+/* Task 2 
+   I initialized the second array of this system. The orders array will be responsible for storing customers order, including
+   the customer name, items ordered, and order status.
+*/
+let orders = [];
